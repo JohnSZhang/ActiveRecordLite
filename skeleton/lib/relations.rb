5aaa89d79c2de674a6d677dd5b31ed057e87b7b9
John Zhang <john.shimin.zhang@gmail.com>
2014-09-28 23:10:09 -0400

    add group,having, select and trying to refactor out build clauses

--- skeleton/lib/relations.rb
+++ skeleton/lib/relations.rb
@@ -9,14 +9,24 @@ class Relation
     self.params[:select] = ["#{@parent.table_name}.*"]
     self.params[:from] = [ @parent.table_name ]
     self.params[:where] = {}
-
+    self.params[:having] = []
+    @query = {}
   end
 
   def method_missing(name, *args)
     self.eval.send(name, *args)
   end
 
-  def joins()
+  def joins(*params)
+    if params.count = 1 && params[0].is_a?(String)
+      self.params[:joins] << params[0]
+    elsif params.count = 1 && params.is_a?(Symbol) 
+      #add join and find foreign keys and tables
+    elsif params.all?{ |el| el.is_a?(Symbol) }
+      #add multiple joins on multiple keys and tables. 
+    else
+      self.argument_error
+    end
     self
   end
 
@@ -25,16 +35,42 @@ class Relation
     self
   end
 
-  def select()
+  def select(params)
+    if params.is_a?(String)
+      self.params[:select] = [params]
+    elsif params.is_a?(Symbol)
+      self.params[:select] = [params.to_s]
+    elsif params.is_a?(Array)
+      self.params[:select] = params.map { |el| el.to_s }.join(', ')
+    else
+      raise self.argument_error
+    end
     self
   end
 
-  def group()
+  def group(params)
+    if params.is_a?(String)
+      self.params[:group].merge!(params)
+    elsif params.is_a?(Symbol)
+      self.params[:group] << params.to_s
+    elsif params.is_a?(Array)
+      self.params[:group] << (params.map{ |el| el.to_s }.join(', '))
+    else
+      raise self.argument_error
+    end
     self
   end
 
-  def having()
+  def having(params)
+    self.params[:having] << params
     self
+  end
+  
+  def build_clause
+    if not self.params[:having].nil && self.params[:group].nil?
+      raise "Need To Have A Having Clause" 
+    end
+    
   end
 
   def build
@@ -50,7 +86,6 @@ class Relation
       FROM #{ from_clause }
       #{ "WHERE " + where_clause unless where_clause == "" }
     SQL
-
   end
 
   def eval
@@ -62,4 +97,7 @@ class Relation
     collection
   end
 
+  def argument_error
+    "Invalid Argument Type"
+  end
 end
